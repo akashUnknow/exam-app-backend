615b473c47b6948a3fc9e7e61d2c1092b92c363c
akashUnknow <akash.unknow@gmail.com>
2025-10-11 20:28:14 +0530

    all api need to test

--- exam-app-backend/exam-service/src/main/java/com/examapp/exam_service/ExamServiceApplication.java
+++ exam-app-backend/exam-service/src/main/java/com/examapp/exam_service/ExamServiceApplication.java
@@ -2,10 +2,9 @@ package com.examapp.exam_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ExamServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExamServiceApplication.class, args);
